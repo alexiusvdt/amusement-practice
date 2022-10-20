f335cf3e16b02d8d2565348fe61679e2f88216ae
Alex Johnson <alex.johnson293@gmail.com>
2022-10-20 11:06:19 -0700

    WIP: bugfixing listener

--- js/scripts.js
+++ js/scripts.js
@@ -13,27 +13,44 @@
 //Using the getElementById method, you can only select an element by its ID. 
 //Generally, you should opt for the selector that most clearly gets the job done
 
-  window.addEventListener("load", function() {
-    //declaring variables
-    let form = document.querySelector("form");
-    //creating the submit listener and specifying its function
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-      function hideResultsAndError();
-      //code to hide previous stuff here
-      const age = parseInt(document.querySelector("input#age").value);
-      const height = parseInt(document.querySelector("input#height").value);
-      //start elif tree
-      if (age && height) {
-        if (age >= 12 && height >= 60) {
-          document.querySelector("div#<targetclass>").removeAttribute("class");
-          document.querySelector("div#<targetclass>").removeAttribute("class");
-          document.querySelector("div#<targetclass>").removeAttribute("class");
-
-        }
+window.addEventListener("load", function() {
+  //building reset button
+  function resetClass() {;
+    document.getElementById("swings").setAttribute("class", "hidden");
+    document.getElementById("coaster").setAttribute("class", "hidden");
+    document.getElementById("tower").setAttribute("class", "hidden");
+    document.getElementById("sorry").setAttribute("class", "hidden");  
+    };
+    //building rideselect
+  function rideSelect() {;
+    const age = parseInt(document.querySelector("input#age").value);
+    const height = parseInt(document.querySelector("input#height").value);
+    console.log('value of age', document.querySelector('age'));
+    if (age && height) {
+      if (age >= 12 && height >= 60) {
+        document.getElementById("div#swings").removeAttribute("class");
+        document.getElementById("div#coaster").removeAttribute("class");
+        document.getElementById("div#tower").removeAttribute("class");
+      } else if (age >= 12 || height >= 48) {
+        document.getElementById("div#swings").removeAttribute("class");
+        document.getElementById("div#coaster").removeAttribute("class");
+      } else if (age >= 5) {
+        document.getElementById("div#swings").removeAttribute("class");
+      } else {
+        document.getElementById("div#sorry").removeAttribute("class");
       }
-    // removing the class & preventing default
-    document.querySelector("div#<targetclass>").removeAttribute("class");
+    };
+  };
+    // variable declaration
+  let form = document.querySelector("form");
+  //creating the submit listener and specifying its function
+  form.addEventListener("submit", function(event) {
     event.preventDefault();
+    console.log('about to reset')
+    // resetClass();
+    console.log('about to select')
+    // console.log('current value of div#swings', document.querySelector('.swings').value)
+    rideSelect();
+      console.log('current value of div#swings', document.querySelector('swings'))
     });
   });
